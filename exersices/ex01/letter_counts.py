571141de9ada3001085465b88accd23d29b88c61
Kvin <Kela@nmbu.no>
2019-09-25 16:28:45 +0200

    Used PEP8
    Fixed all the pep8 minor warnings

--- exersices/ex01/letter_counts.py
+++ exersices/ex01/letter_counts.py
@@ -1,18 +1,17 @@
-import collections as c
-#Imported to use Counter() function. A Counter is a dict subclass for counting hashable objects.
-#It is an unordered collection where elements are stored as dictionary keys and their counts are stored as dictionary values.
-#Counts are allowed to be any integer value including zero or negative counts.
+from collections import Counter
+
 
 def letter_freq(text):
-    text = text.lower() #Making string lower case
-    cnt = c.Counter() #adds the counter dictionary
+    text = text.lower()
+    cnt = Counter()  # adds the counter dictionary
     for symbol in text:
-        cnt[symbol] += 1 #For every letter in the string, add it and a 1, if it was already added then plus 1
-    return cnt #Return the dict
+        cnt[symbol] += 1  # For every letter in the string, add it and a 1, if it was already added then plus 1
+    return cnt
+
 
 if __name__ == '__main__':
     text = input('Please enter text to analyse: ')
 
     frequencies = letter_freq(text)
-    for letter, count in sorted(frequencies.items()): #using preexisting function "sorted" to sort alphabetically
+    for letter, count in sorted(frequencies.items()):
         print('{:3}{:10}'.format(letter, count))
--- exersices/ex01/tidy_code.py
+++ exersices/ex01/tidy_code.py
@@ -6,30 +6,33 @@ __email__ = '@nmbu.no'
 
 def your_guess():
     c = 0
-    while c < 1: #This while loop prevents negative numbers and strings. It forces you to answer positive numbers.
+    while c < 1:  # This while loop prevents negative numbers and strings. It forces you to answer positive numbers.
         c = int(input('Your guess between 1-10: '))
     return c
 
+
 def random_nr():
-    return r(1, 6) + r(1, 6) #generates a random number from 1-5 twice, and then adds them togheter
+    return r(1, 6) + r(1, 6)  # generates a random number from 1-5 twice, and then adds them togheter
+
 
 def check(f, g):
-    return f == g #Checks if the two numbers are equal each other
+    return f == g  # Checks if the two numbers are equal each other
+
 
 if __name__ == '__main__':
 
     result = False
-    tries = 3 #Amount of tries you have to guess
+    tries = 3  # Amount of tries you have to guess
     random = random_nr()
 
-    while not result and tries > 0: #While result and tries are not bigger than 0
+    while not result and tries > 0:  # While result and tries are not bigger than 0
         guess = your_guess()
-        result = check(random, guess) #Checks if your guess and random generated number is the same
-        if not result: #If the numbers aren't the same
+        result = check(random, guess)  # hecks if your guess and random generated number is the same
+        if not result:  # If the numbers aren't the same
             print('Wrong, try again!')
-            tries -= 1 #A try is taken away from total amount of tries
+            tries -= 1  # A try is taken away from total amount of tries
 
-    if tries > 0: #If you have tries left
-        print('You won {} points.'.format(tries)) #you won the amount of tries left in points
+    if tries > 0:  # If you have tries left
+        print('You won {} points.'.format(tries))  # you won the amount of tries left in points
     else:
-        print('You lost. Correct answer: {}.'.format(random)) #Otherwise you lost. This was the randomly generated number
+        print('You lost. Correct answer: {}.'.format(random))  # Otherwise you lost. Show the randomly generated number
